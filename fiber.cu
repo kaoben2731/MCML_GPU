aafbb17a54cc13b1f72c287857273ea8c0efea7a
Benjamin Kao <r07945005@ntu.edu.tw>
2019-07-16 15:23:34 +0800

    change to 5 layer, to compare with new program

--- fiber.cu
+++ fiber.cu
@@ -26,14 +26,23 @@ void fiber_initialization(Fibers* f, float fiber1_position)
 		}
 		else
 		{
-			/*
-			f[i].radius[1] = 0.0002;      //fiber 1
+			// for UT-Austin's system also our fNIRS system
+			f[i].radius[1] = collect_r / 20;      //fiber 1   // turn 200um into 10um
 			f[i].NA[1] = NAOfDetector;
 			f[i].position[1] = 0.8;
 			f[i].angle[1] = ANGLE*PI / 180;
-			*/
 
+			f[i].radius[2] = collect_r;      //fuber 2
+			f[i].NA[2] = NAOfDetector;
+			f[i].position[2] = 1.5;
+			f[i].angle[2] = ANGLE*PI / 180;
+
+			f[i].radius[3] = collect_r;      //fiber 3
+			f[i].NA[3] = NAOfDetector;
+			f[i].position[3] = 3.0;
+			f[i].angle[3] = ANGLE*PI / 180;
 			
+			/*
 			f[i].radius[1] = collect_r;      //fiber 1
 			f[i].NA[1] = NAOfDetector;
 			f[i].position[1] = 1.45;
@@ -58,6 +67,7 @@ void fiber_initialization(Fibers* f, float fiber1_position)
 			f[i].NA[5] = NAOfDetector;
 			f[i].position[5] = 4.1;
 			f[i].angle[5] = ANGLE*PI / 180;
+			*/
 
 		}
 
