8c34cc137e36e465bb2d7ee595122ff4cbaf4163
Benjamin Kao <r07945005@ntu.edu.tw>
2019-08-08 11:03:45 +0800

    add the help information

--- main.cu
+++ main.cu
@@ -4,19 +4,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <string>
 
 
 void FreeSimulationStruct(SimulationStruct* sim, int n_simulations);
 int read_mua_mus(SimulationStruct** simulations, char* sim_input, char* tissue_input);
 void DoOneSimulation(SimulationStruct* simulation, int index, char* output, char* fiber1_position); //Wang modified
+void show_usage(string name);
 
 
 int main(int argc, char* argv[])
 {
 	SimulationStruct* simulations;
 	int n_simulations;
-	unsigned long long seed = (unsigned long long) time(NULL);// Default, use time(NULL) as seed
-															  //const int num_sim = atoi(argv[4]);
+	unsigned long long seed = (unsigned long long) time(NULL); // Default, use time(NULL) as seed
+
+	if (argc < 4) {
+		show_usage(argv[0]);
+		return 0;
+	}
+
 	n_simulations = read_mua_mus(&simulations, argv[1], argv[2]); // read the input file
 
 	if (n_simulations == 0)
@@ -49,6 +56,19 @@ int main(int argc, char* argv[])
 
 	//system("PAUSE");
 	return 0;
+}
+
+void show_usage(string name)
+{
+	size_t found = name.find_last_of("/\\");
+	string exename = name.substr(found + 1);
+	cerr << "Usage: " << exename << " sim_set.json input.txt output.txt <option(s)>\n"
+		<< "Options:\n"
+		<< "\t-h,--help\t Show this help message\n"
+		<< "\t-R,\t\t Replay the detected photon after first simulation\n"
+		// << "\t-W,\t\t Doing white Monte Carlo\n"
+		<< "\t-P,\t\t Output the pathlength for each photon, otherwise output the calculated average pathlength\n"
+		<< endl;
 }
 
 void FreeSimulationStruct(SimulationStruct* sim, int n_simulations)
