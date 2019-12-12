dbb49f7c46b8043125538ee0adc7f536d3318bfd
Benjamin Kao <r07945005@ntu.edu.tw>
2019-12-12 15:58:41 +0800

    Update load_binary_pathlength_output.m
    to the version of K2.01

--- utils/load_binary_pathlength_output.m
+++ utils/load_binary_pathlength_output.m
@@ -1,8 +1,7 @@
 % Load the MCML output binary file
-% Benjamin Kao, 20191203
+% Benjamin Kao, 20191212
 
 %% inputs
-% sim_set_file: the file path of MCML input sim_setup.json file
 % summary_file: the file path of MCML output summary.json file
 % SDS_index: which SDS is this pathlength file
 % pathlength_file: the file path of MCML output pathlength.bin file
@@ -10,12 +9,12 @@
 %% outputs
 % output: the content of the pathlength file
 
-function output=load_binary_pathlength_output(sim_set_file,summary_file,SDS_index,pathlength_file)
-sim_set=jsondecode(fileread(sim_set_file));
+function output=load_binary_pathlength_output(summary_file,SDS_index,pathlength_file)
 sim_sum=jsondecode(fileread(summary_file));
 
+assert(SDS_index<=sim_sum.num_SDS,'SDS_index out of range!');
 fid=fopen(pathlength_file);
-output=fread(fid,[sim_set.number_layers+2 sim_sum.SDS_detected_number(SDS_index)],'float');
+output=fread(fid,[sim_sum.number_layers+2 sim_sum.SDS_detected_number(SDS_index)],'float');
 fclose all;
 output=output';
 end
